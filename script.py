5f8ae4910db49a860fd8f96f8d11d12f4cbe4d82
rf <rf.fa17@gmail.com>
2019-03-09 20:28:42 +0330

    replace try & except statement to if condition

--- script.py
+++ script.py
@@ -9,17 +9,29 @@ def print_evens():
     print('You must enter a number to print even nums from 0 to that\n')
     start = 0
     inp = raw_input('> ')
-    n=int(inp)
 
-
-    if type(n) == type(1):
+    try:
+        n=int(inp)
         for i in range((n/2)+1):
             print(start)
             start += 2
 
-    else:
-        print('You must enter an integer')
+    except ValueError:
+        print('\nYou must enter an integer')
         sys.exit(1)
+
+    except:
+        print('Something goes wrong!')
+
+# You can use the else keyword to define a block of code to be executed if no errors were raised:
+    else:
+        print('*** End of the list *** / else keyword')
+
+# The finally block, if specified, will be executed regardless if the try block raises an error or not.
+    finally:
+        print('\nThe program was closed / finally keyword')
+
+
 
 # make usable for both script and module mode
 if __name__ == '__main__':
