5587e66b8903c484a50cb98c45af1c652f4aab71
rf <rf.fa17@gmail.com>
2019-03-09 08:49:40 +0330

    modify script.py functional and usable for both script and module mode

--- script.py
+++ script.py
@@ -1,6 +1,19 @@
-n = int(raw_input('> '))
-start = 0
+from __future__ import print_function
 
-for i in range((n/2)+1):
-    print start
-    start += 2
+def print_evens():
+    '''
+    It's a function to do print even numbers in range input (from 0 to n , contains zero and n)
+    '''
+
+    print('You must enter a number to print even nums from 0 to that\n')
+    n = int(raw_input('> '))
+    start = 0
+
+    for i in range((n/2)+1):
+        print(start)
+        start += 2
+
+
+# make usable for both script and module mode
+if __name__ == '__main__':
+    print_evens()
